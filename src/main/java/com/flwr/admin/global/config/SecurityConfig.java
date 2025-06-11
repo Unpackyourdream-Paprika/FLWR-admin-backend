3b366fe9f65b0f439d21ac87390c0cbb1a8b0f16
juzdalua <bearwcw@naver.com>
2025-06-11 10:31:27 +0900

    Feat: Update JWT interceptor

--- src/main/java/com/flwr/admin/global/config/SecurityConfig.java
+++ src/main/java/com/flwr/admin/global/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.flwr.admin.global.config;
 
-import com.flwr.admin.global.filter.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,17 +14,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.flwr.admin.global.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-  private String[] allowedEndPoints = {
-      "/auth/**"
-  };
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -34,15 +32,14 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .cors(Customizer.withDefaults())
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(allowedEndPoints).permitAll()
-            .anyRequest().authenticated())
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        // .httpBasic(Customizer.withDefaults())
-        .httpBasic(httpBasic -> httpBasic.disable())
-        .formLogin(login -> login.disable());
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(Whitelist.PERMIT_ALL).permitAll()
+                    .anyRequest().authenticated())
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .httpBasic(httpBasic -> httpBasic.disable())
+            .formLogin(login -> login.disable());
 
     return http.build();
   }
@@ -64,3 +61,4 @@ public class SecurityConfig {
   }
 
 }
+
